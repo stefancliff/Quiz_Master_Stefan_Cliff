0ca0ee12d0b96a87c4a53cfe5354414a57a0a850
Stefan Cliff <stefancliff02@gmail.com>
2022-11-22 18:48:10 +0100

    Initial Upload of QuizMaster

---
+++ Quiz_Master/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string Question = "Enter new question text here";
+    
+    public string GetQuestion(){
+        
+        return 
+    }
+}
